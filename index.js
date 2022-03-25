4d8595ecd43c6e3f6dfbace589bc382d6f1a4d10
moki-moki <scrapironscarecrowdumdum@gmail.com>
2022-03-25 23:58:03 +0100

    adding loading screen improving styling and adding images

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+// LOADING ANIMATION
+let distancePerPoint = 0.3;
+let drawFPS = 60;
+var paths = document.querySelectorAll(".p"),
+  length,
+  timer;
+
+const sv = document.querySelector(".sv");
+
+// Drawing logo Loading
+(function () {
+  paths.forEach((path) => {
+    let pathLength = path.getTotalLength();
+    (function startDrawing() {
+      length = 0;
+      timer = setInterval(increseLength, 1000 / drawFPS);
+      path.style.strokeDasharray = [length, pathLength].join(" ");
+    })();
+
+    function increseLength() {
+      length += distancePerPoint;
+      path.style.strokeDasharray = [length, pathLength].join(" ");
+      if (length >= pathLength) {
+        setTimeout(() => {
+          stopDrawingPath();
+        }, 2000);
+        clearTimeout(timer);
+      }
+    }
+
+    const overlay = document.getElementById("overlay");
+
+    function stopDrawingPath() {
+      clearTimeout(timer);
+      path.style.stroke = "";
+      path.style.strokeDasharray = "";
+      overlay.style.opacity = 0;
+
+      // document.body.style.overflow = "auto";
+      setTimeout(() => {
+        overlay.remove();
+      }, 1300);
+    }
+  });
+})();
+
+//  NAVBAR
 const navBtn = document.querySelector(".navLineContainer");
 const navLinks = document.querySelector(".navLinkWrapper");
 const toggleThemeBtn = document.querySelector(".toggleContainer");
@@ -5,42 +52,53 @@ const dot = document.querySelector(".dot");
 const logo = document.querySelector(".logo");
 const target = document.querySelectorAll(".paralax");
 
-const toggleTheme = () => {
-  dot.classList.toggle("activeTheme");
-  document.body.classList.toggle("darkTheme");
-};
-
-toggleThemeBtn.addEventListener("click", toggleTheme);
-
+// OPEN / CLOSE NAVBAR
 const toggleNav = () => {
   navBtn.classList.toggle("activeNav");
   navLinks.classList.toggle("active");
   // Change color of toggle theme btn when opening nav
   toggleThemeBtn.classList.toggle("navActive");
   logo.classList.toggle("logoActive");
+  if (navBtn.classList.contains("activeNav")) {
+    document.body.scroll = "no";
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.scroll = "yes";
+    document.body.style.overflow = "auto";
+  }
+};
+
+// THEME
+const toggleTheme = () => {
+  dot.classList.toggle("activeTheme");
+  document.body.classList.toggle("darkTheme");
 };
 
 navBtn.addEventListener("click", toggleNav);
+toggleThemeBtn.addEventListener("click", toggleTheme);
 
 // Hie navbar on scroll
-let prevScroll = window.scrollY;
 const nav = document.querySelector("nav");
+let prevScroll = window.pageYOffset;
+let distance = 0;
+let offset = 200;
 
 window.onscroll = () => {
-  let currentScrollPos = window.scrollY;
+  let currentScrollPos = window.pageYOffset;
 
-  if (prevScroll > currentScrollPos) {
-    nav.style.top = "0";
+  if (prevScroll < currentScrollPos) {
+    if (currentScrollPos > distance + offset) {
+      nav.style.top = "-100px";
+    }
   } else {
-    nav.style.top = "-100px";
+    nav.style.top = "0px";
+    distance = window.pageYOffset;
   }
 
-  prevScroll = window.scrollY;
+  prevScroll = currentScrollPos;
 };
 
-// Draw line on scroll
-console.log(path);
-
+// Draws svg middle line
 const drawLine = () => {
   let path = document.getElementById("path");
   let pathLength = path.getTotalLength();
@@ -84,3 +142,32 @@ const drawLine = () => {
 };
 
 window.addEventListener("scroll", drawLine);
+
+// Close nav on link click
+const links = document.querySelectorAll(".navLink");
+
+links.forEach((link) => {
+  console.log(link);
+  link.addEventListener("click", toggleNav);
+  nav.style.top = "-100px";
+  toggleNav();
+});
+
+const blocks = document.querySelectorAll(".container .block");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+      }
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+
+blocks.forEach((block) => {
+  observer.observe(block);
+});

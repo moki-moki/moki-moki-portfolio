fad3e3ddee8caf4b7f4ee68337df23956d9487cc
moki-moki <scrapironscarecrowdumdum@gmail.com>
2022-03-27 07:05:53 +0200

    deleteing drawing svg line adding loader and improving styling

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var paths = document.querySelectorAll(".p"),
   length,
   timer;
 
-const sv = document.querySelector(".sv");
+// Overlay loading
+const overlay = document.getElementById("overlay");
 
 // Drawing logo Loading
 (function () {
@@ -28,15 +29,12 @@ const sv = document.querySelector(".sv");
       }
     }
 
-    const overlay = document.getElementById("overlay");
-
     function stopDrawingPath() {
       clearTimeout(timer);
       path.style.stroke = "";
       path.style.strokeDasharray = "";
-      overlay.style.opacity = 0;
+      overlay.style.top = "-100%";
 
-      // document.body.style.overflow = "auto";
       setTimeout(() => {
         overlay.remove();
       }, 1300);
@@ -44,41 +42,80 @@ const sv = document.querySelector(".sv");
   });
 })();
 
-//  NAVBAR
+/* ALL VERIABLES */
+
+// Navbar
+const nav = document.querySelector("nav");
 const navBtn = document.querySelector(".navLineContainer");
-const navLinks = document.querySelector(".navLinkWrapper");
+const navLinksWrapper = document.querySelector(".navLinkWrapper");
 const toggleThemeBtn = document.querySelector(".toggleContainer");
-const dot = document.querySelector(".dot");
 const logo = document.querySelector(".logo");
+const links = document.querySelectorAll(".navLink");
+
+// Parallax
 const target = document.querySelectorAll(".paralax");
 
-// OPEN / CLOSE NAVBAR
+// Cursor
+const dot = document.querySelector(".dot");
+const mouseCursor = document.querySelector(".cursor");
+
+// Section two projce containers animation
+const blocks = document.querySelectorAll(".block");
+
+// Images
+const images = document.querySelectorAll(".projectImage");
+
+// Buttons
+const btns = document.querySelectorAll("button");
+
+/* FUNCTIONS */
+
+// Open / Close Navbar
 const toggleNav = () => {
+  // Toggles navbar lines
   navBtn.classList.toggle("activeNav");
-  navLinks.classList.toggle("active");
+  // Opens navbar
+  navLinksWrapper.classList.toggle("active");
   // Change color of toggle theme btn when opening nav
   toggleThemeBtn.classList.toggle("navActive");
   logo.classList.toggle("logoActive");
+  mouseCursor.classList.toggle("navActive");
+
+  // Stops scrolling if nav is active
   if (navBtn.classList.contains("activeNav")) {
     document.body.scroll = "no";
     document.body.style.overflow = "hidden";
   } else {
     document.body.scroll = "yes";
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = "unset";
   }
 };
 
-// THEME
+// Theme
+let theme = JSON.parse(localStorage.getItem("theme"));
+
+if (theme) {
+  document.body.classList.add("darkTheme");
+} else {
+  document.body.classList.remove("darkTheme");
+}
+
 const toggleTheme = () => {
-  dot.classList.toggle("activeTheme");
+  // Changes root styles in body
   document.body.classList.toggle("darkTheme");
+
+  // Moves toggle dot
+  dot.classList.toggle("activeTheme");
+
+  // If body contains dark theme set it in local, else remove it
+  if (document.body.classList.contains("darkTheme")) {
+    localStorage.setItem("theme", JSON.stringify("darkTheme"));
+  } else {
+    localStorage.removeItem("theme");
+  }
 };
 
-navBtn.addEventListener("click", toggleNav);
-toggleThemeBtn.addEventListener("click", toggleTheme);
-
 // Hie navbar on scroll
-const nav = document.querySelector("nav");
 let prevScroll = window.pageYOffset;
 let distance = 0;
 let offset = 200;
@@ -98,32 +135,8 @@ window.onscroll = () => {
   prevScroll = currentScrollPos;
 };
 
-// Draws svg middle line
-const drawLine = () => {
-  let path = document.getElementById("path");
-  let pathLength = path.getTotalLength();
-
-  path.style.strokeDasharray = pathLength + " " + pathLength;
-  path.style.strokeDashoffset = pathLength;
-
-  // % of line
-  const scrollPrecentage =
-    (document.documentElement.scrollTop + document.body.scrollTop) /
-    (document.documentElement.scrollHeight -
-      document.documentElement.clientHeight);
-
-  const drawLength = pathLength * scrollPrecentage;
-
-  path.style.strokeDashoffset = pathLength - drawLength + 45;
-
-  // When complete, remove the dash array, otherwise shapte isn't quite sharp
-  if (scrollPrecentage >= 0.99) {
-    path.style.strokeDasharray = "none";
-  } else {
-    path.style.strokeDasharray = pathLength + " " + pathLength;
-  }
-
-  // PARALAX
+// parallax text
+const parallax = () => {
   let length = target.length;
 
   for (let i = 0; i < length; i++) {
@@ -141,22 +154,16 @@ const drawLine = () => {
   }
 };
 
-window.addEventListener("scroll", drawLine);
-
 // Close nav on link click
-const links = document.querySelectorAll(".navLink");
-
 links.forEach((link) => {
-  console.log(link);
   link.addEventListener("click", toggleNav);
   nav.style.top = "-100px";
   toggleNav();
 });
 
-const blocks = document.querySelectorAll(".container .block");
-
+// Observer for section two container animations
 const observer = new IntersectionObserver(
-  (entries) => {
+  (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("animate");
@@ -164,10 +171,57 @@ const observer = new IntersectionObserver(
     });
   },
   {
-    threshold: 1,
+    threshold: 0.5,
   }
 );
 
 blocks.forEach((block) => {
   observer.observe(block);
 });
+
+// Lazy Loading for images
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("saw");
+        const img = entry.target;
+        const src = img.getAttribute("data-lazy");
+
+        img.setAttribute("src", src);
+
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+images.forEach(lazyLoad);
+
+// Cursor Positioning
+const cursorCircle = (e) => {
+  mouseCursor.style.top = e.clientY + "px";
+  mouseCursor.style.left = e.clientX + "px";
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("cursor-grow");
+  });
+
+  btn.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("cursor-grow");
+  });
+});
+
+/* LISTENERS */
+
+// Move cursor
+window.addEventListener("mousemove", cursorCircle);
+// Navbar toggle
+navBtn.addEventListener("click", toggleNav);
+toggleThemeBtn.addEventListener("click", toggleTheme);
+// Text parallax
+window.addEventListener("scroll", parallax);
